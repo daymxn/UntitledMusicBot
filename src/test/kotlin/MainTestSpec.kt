e9b60310afadd60bd230c1524fa4989086f4e879
Daymon <--unset>
2022-02-19 22:39:49 -0600

    Tests should pass now

--- src/test/kotlin/MainTestSpec.kt
+++ src/test/kotlin/MainTestSpec.kt
@@ -1,11 +1,11 @@
 import com.freedom.HelloWorld
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.shouldBe
 
 class MainTestSpec : WordSpec({
     "HelloWorld" should {
         "return Hello World" {
-            HelloWorld() shouldNotBe  "Hello World"
+            HelloWorld() shouldBe  "Hello World"
         }
     }
 })
