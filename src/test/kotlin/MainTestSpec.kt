6280ef610ca27159c18b5db7b4b4351316fc7ea5
Daymon <17409137+daymxn@users.noreply.github.com>
2022-02-19 22:44:01 -0600

    BAT Workflow added (#1)
    Added automatic gradle building and testing on all PRs.

--- src/test/kotlin/MainTestSpec.kt
+++ src/test/kotlin/MainTestSpec.kt
@@ -5,7 +5,7 @@ import io.kotest.matchers.shouldBe
 class MainTestSpec : WordSpec({
     "HelloWorld" should {
         "return Hello World" {
-            HelloWorld() shouldBe "Hello World"
+            HelloWorld() shouldBe  "Hello World"
         }
     }
 })
